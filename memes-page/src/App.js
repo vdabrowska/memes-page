2fa06b6eff8fb0bdea884e3878687b0d449d9f04
vdabrowska <damaris.dabrowska@gmail.com>
2022-04-04 21:27:21 +0200

    Added folder for components, MemesBoard component and did basic styling

--- memes-page/src/App.js
+++ memes-page/src/App.js
@@ -2,18 +2,21 @@ import './App.css';
 import {BrowserRouter, Router, Route, Link, Routes} from 'react-router-dom'
 import Regular from './pages/Regular'
 import Hot from './pages/Hot'
+import MemesBoard from './components/MemesBoard';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Link to='/regular'>REGULAR </Link>
+      <Link to='/regular'>REGULAR</Link>
       <Link to='/hot'>HOT</Link>
       <Routes>
         <Route path='hot' element={<Hot/>}/>
         <Route path='regular' element={<Regular/>}/>
       </Routes>
       </BrowserRouter>
+
+      <MemesBoard/>
     </div>
   );
 }
---
+++ memes-page/src/components/MemesBoard.jsx
@@ -0,0 +1,6 @@
+const MemesBoard = () => {
+    return(
+        <div className="memesBoard"></div> // może nie div ale chodzi o to "opakowanie" dla memów
+    )
+}
+export default MemesBoard
